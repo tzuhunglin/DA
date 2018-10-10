8ac3d2e8a29968ae7bc576ca3fe646545a5096df
tzuhunglin <alfredlin417@gmail.com>
2018-10-10 21:22:47 +0800

    shellsort comment

--- sort/PartitionSortExample.java
+++ sort/PartitionSortExample.java
@@ -1,61 +1,62 @@
-class ArrayPar
+class ArrayPartition
 {
-	private long[] theArray;
-	private int nElems;
+	private long[] arrPartition;
+	private int numValues;
 
-	public ArrayPar(int max)
+	public ArrayPartition(int max)
 	{
-		theArray = new long[max];
-		nElems = 0;
+		arrPartition = new long[max];
+		numValues = 0;
 	}
 
 	public void insert(long value)
 	{
-		theArray[nElems] = value;
-		nElems++;
+		arrPartition[numValues] = value;
+		numValues++;
 	}
 
 	public int size()
 	{
-		return nElems;
+		return numValues;
 	}
 
-	public void display()
+	public void show()
 	{
-		System.out.print("A=");
-		for(int j=0; j<nElems; j++)
+		System.out.print("Array = ");
+		for(int j=0; j<numValues; j++)
 		{
-			System.out.print(theArray[j]+" ");
+			System.out.print(arrPartition[j]+" ");
 		}
 		System.out.println("");
 	}
 
-	public int partitionIt(int left, int right, long pivot)
+	public int partition(int left, int right, long pivot)
 	{
-		int leftPtr = left -1;
-		int rightPtr = right +1;
+		int leftIndex = left -1;
+		int rightIndex = right +1;
 		while(true)
 		{
-			while(leftPtr<right&&theArray[++leftPtr]<pivot);
-
-
-			while(rightPtr > left&&theArray[--rightPtr]>pivot);
-			if(leftPtr >= rightPtr)
+			while(leftIndex<right&&arrPartition[++leftIndex]<pivot);
+			while(rightIndex > left&&arrPartition[--rightIndex]>pivot);
+			
+			if(leftIndex >= rightIndex)
 			{
 				break;
 			}else{
-				swap(leftPtr, rightPtr);
+				swap(leftIndex, rightIndex);
 			}
 		}
-		return leftPtr;
+		return leftIndex;
 	}
 
-	public void swap(int dex1, int dex2)
+	public void swap(int value1, int value2)
 	{
 		long temp;
-		temp = theArray[dex1];
-		theArray[dex1] = theArray[dex2];
-		theArray[dex2] = temp;
+		temp = arrPartition[value1];
+		arrPartition[value1] = arrPartition[value2];
+		arrPartition[value2] = temp;
+		System.out.println("swap("+arrPartition[value2]+","+arrPartition[value1]+")");
+
 	}
 }
 
@@ -63,28 +64,95 @@ class PartitionSortExample
 {
 	public static void main(String[] args)
 	{
-		int maxSize = 16;
-		ArrayPar arr;
-		arr = new ArrayPar(maxSize);
+		ArrayPartition arrTest = new ArrayPartition(20);
 
-		for(int j= 0; j<maxSize; j++)
-		{
-			long n = (int)(java.lang.Math.random()*199);
-			arr.insert(n);
-		}
+		// for(int j= 0; j<maxSize; j++)
+		// {
+		// 	long n = (int)(java.lang.Math.random()*199);
+		// 	arr.insert(n);
+		// }
 
-		arr.display();
-		long pivot = 99;
+		arrTest.insert(2);
+		arrTest.insert(8);
+		arrTest.insert(16);
+		arrTest.insert(9);
+		arrTest.insert(11);
+		arrTest.insert(14);
+		arrTest.insert(3);
+		arrTest.insert(15);
+		arrTest.insert(4);
+		arrTest.insert(12);
+		arrTest.insert(19);
+		arrTest.insert(1);
+		arrTest.insert(18);
+		arrTest.insert(13);
+		arrTest.insert(5);
+		arrTest.insert(7);
+		arrTest.insert(10);
+		arrTest.insert(17);
+		arrTest.insert(0);
+		arrTest.insert(6);
+		
 
-		System.out.print("Pivot is " + pivot);
-		int size = arr.size();
-		int partDex = arr.partitionIt(0, size-1, pivot);
-		System.out.println(", Partition is at index " + partDex);
-		arr.display();
+		arrTest.show();
+		long pivot = 9;
+
+		System.out.println("Pivot is " + pivot);
+		int size = arrTest.size();
+		int parIndex = arrTest.partition(0, size-1, pivot);
+		System.out.println("Partition is at index " + parIndex);
+		arrTest.show();
 	}
 }
 
+/*
+劃分是在排序中所使用的一種分類方式，他不是一種排序法，因為它不會真正的把所有數字完成排序。
+但是我們還是在排序法中看到他的身影，因為有些排序法會需要將先將陣列中的數字先粗略地移動到目標的方向。
 
+劃分的關鍵在於一個樞紐值(pivot)，以這個樞紐值為基準，如果我們要做的事由小到大排列的話，
+那麼劃分的工作就會將比樞紐值小的數字都放到陣列的左方，比樞紐值大的數字則被放到陣列的右方。
+當然，在陣列左方，比樞紐值小的這一群數字和在陣列右方，比樞紐值大的這一群數字都不是有序排列，
+因為劃分的工作僅僅是將這些數字有限的歸類以方便後面真正的排序。
+
+在劃分中，樞紐值(pivot)是最重要的，因為我們需要以他來與所有數進行比較。在劃分中，
+我們會用到兩層while迴圈，外層一個，內層兩個。外層迴圈不斷的執行，直到符合比較完畢的條件。
+而兩個內迴圈的工作則是負責在陣列兩端的位置找出需要交換位置的數值。透過這樣的交換，
+我們可以把比樞紐值小的數字都放在陣列的左邊部分，比樞紐值大的數字都放在陣列的右邊部分。
+
+函式partition就是範例中我們執行劃分的部分。
+變數left和變數right分別是陣列中第一位和最後一位的位置。
+也是我們雙指針leftIndex(左指針)和rightIndex(右指針)在迴圈中的起始位置。
+我們在定義leftIndex和rightIndex都分別讓他們-1和+1超過陣列範圍，
+leftIndex為-1，rightIndex為20，這是為了配和while迴圈中的++leftIndex和--rightIndex。
+雙指針會在迴圈中遞增和遞減往陣列中間方向移動。
+當迴圈開始時，兩個內迴圈會分別在陣列的兩端找尋符合條件的值，左指針找尋比樞紐值大的值，
+右指針找尋比樞紐值小的值，當兩個指針都找到時，這兩個數就會進行交換。透過不斷的找尋和交換，
+所有比樞紐值小的值都會被放在陣列左方，所有比樞紐值大的值都會被方在陣列右方，完成劃分。
+
+範例執行結果如下
+以下為我們一開始輸入的值
+Array = 2 8 16 9 11 14 3 15 4 12 19 1 18 13 5 7 10 17 0 6
+樞紐值設定為9
+Pivot is 9
+左指針在陣列左邊部分發現數字16大於樞紐職9，以及右指針在陣列左邊部分發現數字6小於樞紐值9，
+然後讓他們進行交換
+swap(16,6)
+左指針在陣列左邊部分發現數字9等於樞紐職9，以及右指針在陣列左邊部分發現數字0小於樞紐值9，
+然後讓他們進行交換
+swap(9,0)
+左指針在陣列左邊部分發現數字11等於樞紐職9，以及右指針在陣列左邊部分發現數字7小於樞紐值9，
+然後讓他們進行交換
+swap(11,7)
+左指針在陣列左邊部分發現數字14等於樞紐職9，以及右指針在陣列左邊部分發現數字5小於樞紐值9，
+然後讓他們進行交換
+swap(14,5)
+左指針在陣列左邊部分發現數字15等於樞紐職9，以及右指針在陣列左邊部分發現數字1小於樞紐值9，
+然後讓他們進行交換
+swap(15,1)
+最後回傳樞紐值劃分的位置，也就是在陣列位置9的左方為小於樞紐值得數字，右方圍大於以及等於樞紐值得數字。
+Partition is at index 9
+Array = 2 8 6 0 7 5 3 1 4 12 19 15 18 13 14 11 10 17 9 16
+*/
 
 
 
--- sort/ShellSortExample.java
+++ sort/ShellSortExample.java
@@ -1,55 +1,56 @@
-class ArraySh
+class ArrayShell
 {
-	private long[] theArray;
-	private int nElems;
+	private long[] arrShell;
+	private int numValues;
 
-	public ArraySh(int max)
+	public ArrayShell(int max)
 	{
-		theArray = new long[max];
-		nElems = 0;
+		arrShell = new long[max];
+		numValues = 0;
 	}
 
 	public void insert(long value)
 	{
-		theArray[nElems] = value;
-		nElems++;
+		arrShell[numValues] = value;
+		numValues++;
 	}
 
-	public void display(){
-		System.out.print("A=");
-		for(int j=0; j<nElems; j++)
+	public void show(){
+		System.out.print("Array =");
+		for(int j=0; j<numValues; j++)
 		{
-			System.out.print(theArray[j]+" ");
+			System.out.print(arrShell[j]+" ");
 		}
 		System.out.println(" ");
 	}
 
 	public void shellSort()
 	{
-		int inner, outer;
+		int in, out;
 		long temp;
 
-		int h=1;
-		while(h<= nElems/3)
+		int h=1;//間隔;
+		while(h<= numValues/3)
 		{
 			h= h*3 +1;
 		}
 
-		while(h>0)
+		while(h>0)//當間隔大於0
 		{
-			for(outer=h; outer<nElems; outer++)
+			for(out=h; out<numValues; out++)
 			{
-				temp = theArray[outer];
-				inner = outer;
+				temp = arrShell[out];
+				in = out;
 
-				while(inner > h-1 && theArray[inner-h]>=temp)
+				while(in > h-1 && arrShell[in-h]>=temp)//in > h-1用來做結束判斷，
 				{
-					theArray[inner] = theArray[inner-h];
-					inner -=h;
+					arrShell[in] = arrShell[in-h];
+					in -=h;
 				}
-				theArray[inner] = temp;
+				arrShell[in] = temp;
 			}
 			h = (h-1)/3;
+			show();
 		}
 	}
 }
@@ -58,22 +59,135 @@ class ShellSortExample
 {
 	public static void main(String[] args)
 	{
-		int maxSize = 10;
-		ArraySh arr;
-		arr = new ArraySh(maxSize);
+		ArrayShell arrTest = new ArrayShell(20);
 
-		for(int j=0; j<maxSize;j++)
-		{
-			long n = (int)(java.lang.Math.random()*99);
-			arr.insert(n);
-		}
+		// for(int j=0; j<maxSize;j++)
+		// {
+		// 	long n = (int)(java.lang.Math.random()*99);
+		// 	arr.insert(n);
+		// }
+		arrTest.insert(2);
+		arrTest.insert(8);
+		arrTest.insert(16);
+		arrTest.insert(9);
+		arrTest.insert(11);
+		arrTest.insert(14);
+		arrTest.insert(3);
+		arrTest.insert(15);
+		arrTest.insert(4);
+		arrTest.insert(12);
+		arrTest.insert(19);
+		arrTest.insert(1);
+		arrTest.insert(18);
+		arrTest.insert(13);
+		arrTest.insert(5);
+		arrTest.insert(7);
+		arrTest.insert(10);
+		arrTest.insert(17);
+		arrTest.insert(0);
+		arrTest.insert(6);
 
-		arr.display();
-		arr.shellSort();
-		arr.display();
+		arrTest.show();
+		arrTest.shellSort();
+		// arrTest.show();
 	}
 }
+/*
+希爾排序可以說是插入排序的加強版，原本的插入排序只往前一格作比較，
+這讓排在陣列右端的小數目需要不斷一格一格的往前比較然後往後移動那些比她大的數字，
+這會增加排序的時間。例如現在有十個數字做排序，恰巧最小數在最後一位，
+那麼在最後一圈時他就需要跟前面九個數字做比較並且移動那九個數字，這是很沒有效率的做法。
+而希爾排序基於插入排序，針對的這個缺點做了改良。
 
+希爾排序與插入排序的差異點就在於兩組值間隔數，有了這個間格數，
+希爾排序可以讓排在陣列後端的小數目大幅度且快速的移動到陣列前方，
+在這個範例中我們所使用的間隔數計算法是 h = h*3+1。這不是唯一的計算法，
+有興趣的話可以在網路上找到其他間格數的計算方式。
+
+shellSort是我們執行希爾排序的函式，一開始最重要的步驟就是先計算我們要使用的間隔數。
+int h=1;
+while(h<= numValues/3)
+{
+	h= h*3 +1;
+}
+while迴圈的條件是當我們的陣列長度除以3大於我們的間隔數時，我們就執行h= h*3 +1，
+然後再做一次判斷，直到條件不符合才停止。h的初始值為1，
+重複執行h= h*3 +1我們會得到序列1,4,13,121.....
+序列中的數字代表著兩組進行比較的數字的間隔，如果我們得到的間隔數是13，
+那麼我們一開始將會比較陣列中的第13位和第0位然後第14位和第1位......
+在某些狀況下，部分中間的數字是不會在間隔中被比較到的。
+在比較完13的間隔後，我們會在下面比較迴圈中做縮小間隔的動作h = (h-1)/3，依以上間隔數13的例子，
+間隔數將會被縮小為4，這時候會由陣列中的第4位和第0位進行比較然後第5位和第1位進行比較。
+當比較的位置到了第8位它將會先和第4位做比較然後和第0位做比較，第9位時它將會先和第5位做比較然後和第1位做比較。
+如果到了第16位，間隔是4，那他將會和第12位、第8位、第4位、第0位做比較，
+如果遇到比她小的值那就把它們往後移動騰出位置，如果遇到比她大的值或是前方已經沒有值可以比較時那就停下來插入空格。
+當間隔數被縮小到1時，那就等於執行基本的插入排序法。只是這時候小的數目都已經被移動到了陣列前端，
+大的數目都被移動到了陣列的後端，
+所以不會發生一般插入排序中所發生的小數目需要比較以及移動陣列中大部分的值才可以到達他所需要到的位置。
+
+for(out=h; out<numValues; out++)
+{
+	temp = arrShell[out];
+	in = out;
+
+	while(in > h-1 && arrShell[in-h]>=temp)
+	{
+		arrShell[in] = arrShell[in-h];
+		in -=h;
+	}
+	arrShell[in] = temp;
+}
+h = (h-1)/3;
+
+執行希爾排序一共需要三層迴圈，最外圈是判斷間隔值，最小的間隔式1，所以必須大於0。
+中層迴圈的指標是比較的起始點，會由間隔數開始，然後在每一圈遞增。
+在每一圈中我們會把起始點的值放入temp變數中用來比較，然後將起始點的位置放入內迴圈的指標in中。
+內回圈是判斷需要用來比較的位置(in-h)、進行比較以及比較後是否需要將前面的數往後h個位置移動。
+當條件成立移動位置後，我們便會執行in -=h，這表示in指標會再往前h個位置，如果位置不存在(in小於0)便會停止迴圈，
+然後將temp變數得值插入適當的位置。當中層回圈結束後便再次縮小間隔。
+
+範例執行結果如下
+以下為我們一開始輸入的值
+Array =2 8 16 9 11 14 3 15 4 12 19 1 18 13 5 7 10 17 0 6
+當外迴圈第一圈，間隔為13結束後
+Array =2 5 7 9 11 0 3 15 4 12 19 1 18 13 8 16 10 17 14 6
+在這一圈中比較得的組數分別是
+位置13 & 位置0
+位置14 & 位置1
+位置15 & 位置2
+位置16 & 位置3
+位置17 & 位置4
+位置18 & 位置5
+位置19 & 位置6
+檢查上述的組數可以發現這些經過比較的組數都已經過排列。
+在這邊我們可以發現位置7~12的數字並沒有進行比較，不過沒有關係，因為他們將會在下一圈中進行比較。
+
+當外迴圈第二圈，間隔為4結束後
+Array =2 0 3 1 4 5 7 6 10 12 8 9 11 13 14 15 18 17 19 16
+在這一圈中比較得的組數分別是
+位置4 & 位置0
+位置5 & 位置1
+位置6 & 位置2
+位置7 & 位置3
+位置8 & 位置4 & 位置0
+位置9 & 位置5 & 位置1
+位置10 & 位置6 & 位置2
+位置11 & 位置7 & 位置3
+位置12 & 位置8 & 位置4 & 位置0
+位置13 & 位置9 & 位置5 & 位置1
+位置14 & 位置10 & 位置6 & 位置2
+位置15 & 位置11 & 位置7 & 位置3
+位置16 & 位置12 & 位置8 & 位置4 位置0
+位置17 & 位置13 & 位置9 & 位置5 位置1
+位置18 & 位置14 & 位置10 & 位置6 位置2
+位置19 & 位置15 & 位置11 & 位置7 位置3
+
+
+
+當外迴圈第三圈，間隔為1結束後。這時候經過間隔1的排序(基本的插入排序)他已經變成完全有序了。
+Array =0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+
+*/
 
 
 
