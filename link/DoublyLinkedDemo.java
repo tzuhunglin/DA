3e734e38eb5a672123fcb9b1dd256aa797fcfe00
tzuhunglin <alfredlin417@gmail.com>
2018-10-29 08:37:37 +0800

    ddddd

--- link/DoublyLinkedDemo.java
+++ link/DoublyLinkedDemo.java
@@ -34,129 +34,129 @@ class DoublyLinkedList
 	public void insertFirst(long dd)
 	{
 		Link newLink = new Link(dd);
-		if(isEmpty())
+		if(isEmpty())//當鍊表為空時
 		{
-			last = newLink;
+			last = newLink;//新結點為唯一結點，所以將last也指向新結點
 		}
-		else
+		else//當鍊表不為空時
 		{
-			first.previous = newLink;
+			first.previous = newLink;//將first所指向物件的previous屬性指向新結點
 		}
 
-		newLink.next = first;
-		first = newLink;
+		newLink.next = first;//將新結點所指向的物件的next屬性指向原本first所指向的物件
+		first = newLink;//將first指向新結點
 	}
 
 	public void insertLast(long dd)
 	{
 		Link newLink = new Link(dd);
-		if(isEmpty())
+		if(isEmpty())//當鍊表為空時
 		{
-			first = newLink;
+			first = newLink;//新結點為唯一結點，所以將first也指向新結點
 		}
-		else
+		else//當鍊表不為空時
 		{
-			last.next = newLink;
-			newLink.previous = last;
+			last.next = newLink;//將last所指向物件的next屬性指向新結點
+			newLink.previous = last;//將新結點的previous屬性指向原本last所指向的物件
 		}
 
-		last = newLink;
+		last = newLink;//將last指向新結點
 	}
 
 	public Link deleteFirst()
 	{
-		Link temp = first;
+		Link temp = first;//將temp指向原本first所指向的物件
 
-		if(first.next == null)
+		if(first.next == null)//如果first所指向物件的next屬性為null(沒有結點)
 		{
-			last = null;
+			last = null;//將last設為null
 		}
-		else
+		else//如果first所指向物件的next屬性不為null(有結點)
 		{
-			first.next.previous = null;
+			first.next.previous = null;//將first所指向的物件的next屬性所指向的的物件的previous屬性設為null
 		}
 
-		first = first.next;
-		return temp;
+		first = first.next;//將first指向原本first所指向物件的next屬性所指向的物件
+		return temp;//回傳指向原本first所指向的物件
 	}
 
 	public Link deleteLast()
 	{
-		Link temp = last;
-		if(first.next == null)
+		Link temp = last;//將temp指向原本last所指向的物件
+		if(first.next == null)//如果first所指向物件的next屬性為null(沒有結點)
 		{
-			first = null;
+			first = null;//將first設為null
 		}
-		else
+		else//如果first所指向物件的next屬性不為null(有結點)
 		{
-			last.previous.next = null;
+			last.previous.next = null;//將last所指向的物件的previous屬性所指向的的物件的next屬性設為null
 		}
-		last = last.previous;
-		return temp;
+		last = last.previous;//將last指向原本last所指向物件的previousㄒ屬性所指向的物件
+		return temp;//回傳指向原本last所指向的物件
 	}
 
 	public boolean insertAfter(long key, long dd)
 	{
-		Link current = first;
-		while(current.dData != key)
+		Link current = first;//將current指向first所指向的物件
+		while(current.dData != key)	//當還沒找到目標節點時迴圈繼續執行
 		{
-			current = current.next;
-			if(current == null)
+			current = current.next;//將current指向current.next所指向的物件
+			if(current == null)//一直找到最後一個節點的next
 			{
-				return false;
+				return false;//返回錯誤
 			}
 		}
 
-		Link newLink = new Link(dd);
+		Link newLink = new Link(dd);//建立新節點
 
-		if(current == last)
+		if(current == last)//如果current所指向的物件為last所指向的物件
 		{
-			newLink.next = null;
-			last = newLink;
+			newLink.next = null;//新節點將被last所指向，他的next設為null
+			last = newLink;//將last指向新結點物件
 		}
-		else
+		else//如果current不為last所指向的物件
 		{
-			newLink.next = current.next;
+			newLink.next = current.next;//新結點的next屬性指向current的next屬性所指向的物件
 
-			current.next.previous = newLink;
+			current.next.previous = newLink;//將current所指向物件的next屬性所指向物件的previous屬性指向新結點
 		}
 
-		newLink.previous = current;
-		current.next = newLink;
+		newLink.previous = current;//將新結點的previous屬性指向current所指向的物件
+		current.next = newLink;//將current的next屬性指向心結點
 		return true;
 	}
 
 	public Link deleteKey(long key)
 	{
-		Link current = first;
-		while(current.dData != key)
+		Link current = first;//將current指向first所指向的物件
+		while(current.dData != key)//當還沒找到目標節點時迴圈繼續執行
 		{
-			current = current.next;
-			if(current == null)
+			current = current.next;//將current指向current.next所指向的物件
+			if(current == null)//一直找到最後一個節點的next
 			{
-				return null;
+				return null;//返回錯誤
 			}
 		}
 
-		if(current==first)
+		if(current==first)//如果current所指向的物件為first所指向的物件
 		{
-			first = current.next;
+			first = current.next;//將first指向current所指向物件的next屬性所指向的物件
 		}
-		else
+		else//如果current所指向的物件不為first所指向的物件
 		{
-			current.previous.next = current.next;
-		}
-
-		if(current==last)
-		{
-			last = current.previous;
-		}
-		else
-		{
-			current.next.previous = current.previous;
+			current.previous.next = current.next;//將current所指向物件的previous屬性所指向的物件的next屬性指向current所指向物件的next屬性所指向的物件
 		}
 
-		return current;
+		if(current==last)//如果current所指向的物件為last所指向的物件
+		{
+			last = current.previous;//將last指向current所指向物件的previous屬性所指向的物件
+		}
+		else//如果current所指向的物件不為last所指向的物件
+		{
+			current.next.previous = current.previous;//將current所指向物件的next屬性所指向物件的previous屬性指向將current所指向物件的previous屬性所指向的物件
+		}
+
+		return current;//返回current節點
 	}
 	public void displayForward()
 	{
@@ -221,7 +221,196 @@ class DoublyLinkedDemo
 
 
 
+/*
+無論是傳統的鍊表還是雙端鍊表他們都可以讓指標很輕易的從一個節點到達下一個節點，
+但是如果需要讓指標從一個節點一動到上一個節點就必須讓指標再從first的位置從新遍歷，
+一步一步的移動到原本目標結點的上一層。
+這種從first開始查找方式效率非常低也讓某些鍊錶的應用上出現問題。
 
+雙端鍊表提供了向上遍歷的功能，就是讓每個節點增加了一個屬性previous，
+讓一個節點可以向上做連結，讓㽪個鍊表可以由last的位置向上遍歷至first的位置。
+
+雙端鍊錶的缺點是在插入或著是刪除一個節點時需要處理四個引用(傳統鍊錶為兩個)，兩個連接前一個節點，
+兩個連接後一個節點。當然，在處理更多引用的狀況下，鍊結點所佔的空間也更大了。
+
+public DoublyLinkedList()
+{
+	first = null;
+	last = null;
+}
+
+雙項鍊表和雙端鍊表一樣，我們會有兩個屬性first和last。一個指向表頭，一個指向表尾。
+
+public void insertFirst(long dd)
+{
+	Link newLink = new Link(dd);
+	if(isEmpty())//當鍊表為空時
+	{
+		last = newLink;//新結點為唯一結點，所以將last也指向新結點
+	}
+	else//當鍊表不為空時
+	{
+		first.previous = newLink;//將first所指向物件的previous屬性指向新結點
+	}
+
+	newLink.next = first;//將新結點所指向的物件的next屬性指向原本first所指向的物件
+	first = newLink;//將first指向新結點
+}
+
+insertFirst為由表頭插入新結點的函式，一開始我們先宣告一個新結點。
+接著判斷鍊表是否為空，如果鍊表為空，那表示我們需要將last也指向這個唯一的節點。
+如果鍊表不為空，我們就將原本first所指向物件的previous屬性指向新結點，
+讓新結點排在first所指向物件的上一層。
+接著再將新結點的next屬性指向first所指向的物件。
+最後我們要將first指向新節點。
+
+public void insertLast(long dd)
+{
+	Link newLink = new Link(dd);
+	if(isEmpty())//當鍊表為空時
+	{
+		first = newLink;//新結點為唯一結點，所以將first也指向新結點
+	}
+	else//當鍊表不為空時
+	{
+		last.next = newLink;//將last所指向物件的next屬性指向新結點
+		newLink.previous = last;//將新結點的previous屬性指向原本last所指向的物件
+	}
+
+	last = newLink;//將last指向新結點
+}
+
+insertLast為由表尾插入新結點的函式，一開始我們先宣告一個新結點。
+接著判斷鍊表是否為空，如果鍊表為空，那表示我們需要將first也指向這個唯一的節點。
+如果鍊表不為空，我們就將原本last所指向物件的next屬性指向新結點，
+讓新結點排在last所指向物件的下一層。
+接著再將新結點的previous屬性指向last所指向的物件。
+最後我們要將last指向新節點。
+
+public Link deleteFirst()
+{
+	Link temp = first;//將temp指向原本first所指向的物件
+
+	if(first.next == null)//如果first所指向物件的next屬性為null(沒有結點)
+	{
+		last = null;//將last設為null
+	}
+	else//如果first所指向物件的next屬性不為null(有結點)
+	{
+		first.next.previous = null;//將first所指向的物件的next屬性所指向的的物件的previous屬性設為null
+	}
+
+	first = first.next;//將first指向原本first所指向物件的next屬性所指向的物件
+	return temp;//回傳指向原本first所指向的物件
+}
+
+deleteFirst為刪除表頭節點的函式，首先我們先將temp指向原本first所指向的物件。
+接著判斷first的下一層是否為空，如果下一層為空則表示目前只有一個節點。
+刪除後鍊錶就通通為空，所以將last設為null。
+如果first的下一層還有節點，就將下一層的previous設為null(因為first要被刪除)。
+最後用原本first下一層的物件取代原本的first，再回傳被刪除的節點。
+
+public Link deleteLast()
+{
+	Link temp = last;//將temp指向原本last所指向的物件
+	if(first.next == null)//如果first所指向物件的next屬性為null(沒有結點)
+	{
+		first = null;//將first設為null
+	}
+	else//如果first所指向物件的next屬性不為null(有結點)
+	{
+		last.previous.next = null;//將last所指向的物件的previous屬性所指向的的物件的next屬性設為null
+	}
+	last = last.previous;//將last指向原本last所指向物件的previousㄒ屬性所指向的物件
+	return temp;//回傳指向原本last所指向的物件
+}
+
+deleteLast為刪除表尾節點的函式，首先我們先將temp指向原本last所指向的物件。
+接著判斷first的下一層是否為空，如果下一層為空則表示目前只有一個節點。
+刪除後鍊錶就通通為空，所以將last設為null。
+如果first的下一層還有節點，
+就將last下一層節點的previous屬性設為null(因為last要被刪除)。
+最後用原本last上一層的物件取代原本的last，再回傳被刪除的節點。
+
+public boolean insertAfter(long key, long dd)
+{
+	Link current = first;//將current指向first所指向的物件
+	while(current.dData != key)	//當還沒找到目標節點時迴圈繼續執行
+	{
+		current = current.next;//將current指向current.next所指向的物件
+		if(current == null)//一直找到最後一個節點的next
+		{
+			return false;//返回錯誤
+		}
+	}
+
+	Link newLink = new Link(dd);//建立新節點
+
+	if(current == last)//如果current所指向的物件為last所指向的物件
+	{
+		newLink.next = null;//新節點將被last所指向，他的next設為null
+		last = newLink;//將last指向新結點物件
+	}
+	else//如果current不為last所指向的物件
+	{
+		newLink.next = current.next;//新結點的next屬性指向current的next屬性所指向的物件
+
+		current.next.previous = newLink;//將current所指向物件的next屬性所指向物件的previous屬性指向新結點
+	}
+
+	newLink.previous = current;//將新結點的previous屬性指向current所指向的物件
+	current.next = newLink;//將current的next屬性指向心結點
+	return true;
+}
+
+insertAfter的作用在於在特定結點後插入新的節點。首先，我們會先找到新結點所要被插入的位置，
+找到後我們可以將這個功能視為將鍊錶拆為兩段，分別為previous段跟current段。
+previous所指向的結點就是鏈表前段的最後一個節點。
+current所指向的就是鍊錶後段的第一個節點。
+最後的步驟就是將前後兩段分別與新結點連上。
+
+
+
+
+public Link deleteKey(long key)
+{
+	Link current = first;//將current指向first所指向的物件
+	while(current.dData != key)//當還沒找到目標節點時迴圈繼續執行
+	{
+		current = current.next;//將current指向current.next所指向的物件
+		if(current == null)//一直找到最後一個節點的next
+		{
+			return null;//返回錯誤
+		}
+	}
+
+	if(current==first)//如果current所指向的物件為first所指向的物件
+	{
+		first = current.next;//將first指向current所指向物件的next屬性所指向的物件
+	}
+	else//如果current所指向的物件不為first所指向的物件
+	{
+		current.previous.next = current.next;//將current所指向物件的previous屬性所指向的物件的next屬性指向current所指向物件的next屬性所指向的物件
+	}
+
+	if(current==last)//如果current所指向的物件為last所指向的物件
+	{
+		last = current.previous;//將last指向current所指向物件的previous屬性所指向的物件
+	}
+	else//如果current所指向的物件不為last所指向的物件
+	{
+		current.next.previous = current.previous;//將current所指向物件的next屬性所指向物件的previous屬性指向將current所指向物件的previous屬性所指向的物件
+	}
+
+	return current;//返回current節點
+}
+
+deleteKey函式的功能是在練表中刪除特定的節點，首先，我們會先找到我們所要刪除的節點。
+找到之後，如果節點為first或last所指向的節點，我們會需要將這兩個變數從新指向到新結點的位置。
+如果要刪除的節點不為first或last所指向的節點，那麼我們就需要調整目標節點上一層和下一層的節點。
+將他們兩個節點的previous和next屬性互相做連結。
+
+*/
 
 
 
--- link/ListInsertionSortDemo.java
+++ link/ListInsertionSortDemo.java
@@ -33,10 +33,10 @@ class SortedList
 		Link previous = null;
 		Link current = first;
 
-		while(current != null && k.dData > current.dData)
+		while(current != null && k.dData > current.dData)//current!=null表示鏈表不為空，並且key值比結點值大。
 		{
-			previous = current;
-			current = current.next;
+			previous = current;//將previous指向現在current所指向的位置
+			current = current.next;//再將current指向current.next的位置。此時，原鍊錶被分為兩段
 		}
 
 		if(previous==null)
@@ -45,10 +45,10 @@ class SortedList
 		}
 		else
 		{
-			previous.next = k;
+			previous.next = k;//將previous段的next接上NewLink
 		}
 
-		k.next = current;
+		k.next = current;//將newLink.next接上current段
 	}
 
 	public Link remove()
@@ -99,10 +99,81 @@ class ListInsertionSortDemo
 
 
 
+/*
+有序鍊錶在插入新結點時會依照節點中某項特定的值進行排序。
+有序鍊錶在插入新數據時是比有序陣列有優勢的，因為他不需要移動的其他元素，另外，
+鍊表不像陣列有長度的限制。有序鍊錶經常使用於優先及隊列以及堆。在這個範例中，
+我們將使用有序鍊表作為工具協助排序陣列中的數據。
+
+
+在這個範例中我們將會生成一組無序陣列然後將無序陣列交給有序練表的類別生成一組有序練表，
+再有序鍊錶生成後我們會用刪除節點的方式將節點一個一個返回陣列中取代陣列中原有的數據。
+int size = 10;
+
+Link[] linkArray = new Link[size];
+
+for(int j=0; j<size; j++)
+{
+	int n = (int)(java.lang.Math.random()*99);
+	Link newLink = new Link(n);
+	linkArray[j] = newLink;
+}
+
+System.out.print("Unsorted array: ");
+
+一開始我們需要先生成一組無序Link陣列。
+
+SortedList theSortedList = new SortedList(linkArray);
+
+for(int j=0; j<size; j++)
+{
+	linkArray[j] = theSortedList.remove();
+}
+
+System.out.print("Sorted Array:  ");
+
+無序陣列生成後我們將它用來呼叫有序鏈結類別SortedList，呼叫完成後物件theSortedList已經成為一組有序鏈結，
+鏈結中各節點的值都對應陣列中節點的值。然後我們會用刪除的方式依序將鏈表中各節點取代陣列中原有的節點讓陣列變為有序陣列。
+
+public void insert(Link k)
+{
+	Link previous = null;
+	Link current = first;
+
+	while(current != null && k.dData > current.dData)//current!=null表示鏈表不為空，並且key值比結點值大。
+	{
+		previous = current;//將previous指向現在current所指向的位置
+		current = current.next;//再將current指向current.next的位置。此時，原鍊錶被分為兩段
+	}
+
+	if(previous==null)
+	{
+		first = k;
+	}
+	else
+	{
+		previous.next = k;//將previous段的next接上NewLink
+	}
+
+	k.next = current;//將newLink.next接上current段
+}
+
+insert為我們鍊表中插入新結點並排序的函式，在函式中我們會需要兩個變數previous跟current來協助我們進行下面的while迴圈。
+previous預設為null，current預設為first，如果鍊錶為空，first的值也會為空。
+當while迴圈進行時，我們會先判斷current != null && k.dData > current.dData(練表不為空，並且新數據大於current的數據)。
+我們要做的事升冪排列，所以我們比須一直找到比新數據大的值才停下。當還沒找到比新數據大的值時，
+我們需要將變數previous指向現在current所指向的位置，再將current指向current.next的位置。此時，我們可以將鍊錶看作為兩段
+一段為previous段，此段的最後一個節點就是previous。一段為current段，此段的第一個節點為current。
+當我們找到了新結點的位置時，我們會將previous段和current段分別皆在新結點的前後形成有序鍊錶。
 
 
 
 
+有序鍊表再插入或著刪除特定項數據需要O(N)的時間，因為他需要搜尋平均一半的鍊錶節點數量。
+但是在找到最小項或著刪除最小項只需要O(N)的時間。如果我們經常的需要最小的項目，
+並且不需要經常地插入新數據，有序鍊錶會是一個好的選擇，例如堆和優先級隊列。
+
+*/
 
 
 
