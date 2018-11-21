b988b54197845ab004177ad31d3c3e7b8396e8de
tzuhunglin <alfredlin417@gmail.com>
2018-11-22 01:24:40 +0800

    new

--- graph/DFSDemo.java
+++ graph/DFSDemo.java
@@ -45,11 +45,11 @@ class Vertex
 
 class Graph
 {
-	private final int MAX_VERTS = 20;
-	private Vertex vertexList[];
-	private int adjMat[][];
-	private int nVerts;
-	private StackX theStack;
+	private final int MAX_VERTS = 20;//最大頂點數
+	private Vertex vertexList[];//頂點列表
+	private int adjMat[][];//連接頂點的邊
+	private int nVerts;//頂點數量
+	private StackX theStack;//儲列
 
 	public Graph()
 	{
@@ -85,28 +85,28 @@ class Graph
 
 	public void dfs()
 	{
-		vertexList[0].wasVisited = true;
-		displayVertex(0);
-		theStack.push(0);
+		vertexList[0].wasVisited = true;//將第一頂點設為已拜訪
+		displayVertex(0);//印出第一頂點
+		theStack.push(0);//將第一頂點的位置放入儲列
 
-		while(!theStack.isEmpty())
+		while(!theStack.isEmpty())//當儲列還有東西時執行
 		{
-			int v = getAdjUnvistedVertex(theStack.peek());
+			int v = getAdjUnvistedVertex(theStack.peek());//取得臨邊未拜訪的頂點位置
 			if(v==-1)
 			{
 				theStack.pop();
 			}
 			else
 			{
-				vertexList[v].wasVisited = true;
-				displayVertex(v);
-				theStack.push(v);
+				vertexList[v].wasVisited = true;//將頂v點設為已拜訪
+				displayVertex(v);//印出頂點v
+				theStack.push(v);//將頂點v放入儲列
 			}
 		}
 
 		for(int j=0; j<nVerts; j++)
 		{
-			vertexList[j].wasVisited = false;
+			vertexList[j].wasVisited = false;//將所有頂點設為未拜訪
 		}
 	}
 
@@ -114,12 +114,12 @@ class Graph
 	{
 		for(int j=0; j<nVerts; j++)
 		{
-			if(adjMat[v][j] == 1 && vertexList[j].wasVisited == false)
+			if(adjMat[v][j] == 1 && vertexList[j].wasVisited == false)//當v和j的邊存在並且頂點j為未拜訪
 			{
-				return j;
+				return j;//回傳j
 			}
 		}
-		return -1;
+		return -1;//沒有任何符合的條件回傳-1
 
 	}
 }
@@ -148,7 +148,107 @@ class DFSDemo
 	}
 }
 
+/*
+圖是一種跟樹相似的資料結構，或著我們可以說樹是圖的一種，但是樹跟圖的應用方式不同。樹可以用來當做儲存數據的資料結構，
+不過圖卻不用來當作儲存數據的資料結構，而是用來處理一些其他問題。
 
+圖通常有一個固定的形狀，這通常取決於要處理的問題本身的狀況，例如我們可以用圖來表示一個地區中的城市和路線。
+圖中的每一個頂點都表示一個城市，而連結頂點的邊就表示連接這些城市的道路。
+
+在這篇範例中藥演示的是深度優先搜尋，深度優先搜尋就是從一個起點開始，
+沿著與起點連接的頂點一直走到底直到路徑中沒有未經過的頂點為止，然後再換另一個與起點連結的頂點走另一條路，依此類推。
+public static void main(String[] args)
+{
+	Graph theGraph = new Graph();
+	theGraph.addVertex('A');
+	theGraph.addVertex('B');
+	theGraph.addVertex('C');
+	theGraph.addVertex('D');
+	theGraph.addVertex('E');
+
+	theGraph.addEdge(0,1);
+	theGraph.addEdge(1,2);
+	theGraph.addEdge(0,3);
+	theGraph.addEdge(3,4);
+
+	System.out.print("Visits: ");
+	theGraph.dfs();
+	System.out.println();
+
+
+}
+
+在範例的一開始我們先呼叫一個圖，並且建立他的頂點以及連接頂點的邊。
+
+private final int MAX_VERTS = 20;//最大頂點數
+private Vertex vertexList[];//頂點列表
+private int adjMat[][];//連接頂點的邊
+private int nVerts;//頂點數量
+private StackX theStack;//儲列
+
+public Graph()
+{
+	vertexList = new Vertex[MAX_VERTS];
+
+	adjMat = new int[MAX_VERTS][MAX_VERTS];
+	nVerts = 0;
+	for(int j=0; j<MAX_VERTS;j++)
+	{
+		for(int k=0; k<MAX_VERTS; k++)
+		{
+			adjMat[j][k] = 0;
+		}
+	}
+	theStack = new StackX();
+}
+
+呼叫圖時我們會需要先建立圖的基本設置，並且將途中所有路徑adjMat[j][k]先設為0表示路徑不通。
+theGraph.addEdge(?,?)為設置路徑的函示，將頂點參數帶入後這兩個點頂的邊將會被設為1表示兩個頂點相通。
+
+public int getAdjUnvistedVertex(int v)
+{
+	for(int j=0; j<nVerts; j++)
+	{
+		if(adjMat[v][j] == 1 && vertexList[j].wasVisited == false)//當v和j的邊存在並且頂點j為未拜訪
+		{
+			return j;//回傳j
+		}
+	}
+	return -1;//沒有任何符合的條件回傳-1
+
+}
+函式getAdjUnvistedVertex可以幫我們取得與頂點v相臨並且未經拜訪的頂點。
+
+public void dfs()
+{
+	vertexList[0].wasVisited = true;//將第一頂點設為已拜訪
+	displayVertex(0);//印出第一頂點
+	theStack.push(0);//將第一頂點的位置放入儲列
+
+	while(!theStack.isEmpty())//當儲列還有東西時執行
+	{
+		int v = getAdjUnvistedVertex(theStack.peek());//取得臨邊未拜訪的頂點位置
+		if(v==-1)
+		{
+			theStack.pop();
+		}
+		else
+		{
+			vertexList[v].wasVisited = true;//將頂v點設為已拜訪
+			displayVertex(v);//印出頂點v
+			theStack.push(v);//將頂點v放入儲列
+		}
+	}
+
+	for(int j=0; j<nVerts; j++)
+	{
+		vertexList[j].wasVisited = false;//將所有頂點設為未拜訪
+	}
+}
+
+
+函式dfs負責執行深度優先搜尋，他將會由第一個頂點開始出發
+/*
 
 
 
--- heap/HeapSortDemo.java
+++ heap/HeapSortDemo.java
@@ -38,29 +38,32 @@ class Heap
 	public void trickleDown(int index)
 	{
 		int largerChild;
-		Node top = heapArray[index];
-		while(index < currentSize/2)
+		Node top = heapArray[index];//將根結點位置的直放入變數top
+		while(index<currentSize/2)//當指標未到達葉節點階層時執行
 		{
 			int leftChild = 2*index+1;
 			int rightChild = leftChild+1;
 
-			if(rightChild<currentSize && heapArray[leftChild].getKey()< heapArray[rightChild].getKey())
+			if(rightChild < currentSize && heapArray[leftChild].getKey()<heapArray[rightChild].getKey())
+			//當右子節點存在並且右子節點大於左子結點
 			{
 				largerChild = rightChild;
 			}
 			else
+			//當rightChild == currentSize 表示右子節點不存在。
 			{
-				largerChild = leftChild;
+				largerChild = leftChild;//將largerChild設為左子結點
 			}
+
 			if(top.getKey()>= heapArray[largerChild].getKey())
 			{
 				break;
 			}
 
-			heapArray[index] = heapArray[largerChild];
-			index = largerChild;
+			heapArray[index] = heapArray[largerChild];//將比較大的子結點放入父傑點的位置
+			index = largerChild;//將index設為largerChild的值
 		}
-		heapArray[index] = top;
+		heapArray[index] = top;//將top中的節點放入適當的位置
 	}
 
 	public void displayHeap()
@@ -189,7 +192,54 @@ class HeapSortDemo
 	}
 }
 
+/*
+堆排序是基於堆的一種排序法，他很間單，又很有效率。堆排序的方法就是使用最簡單的插入法將數據插入堆陣列中(所有數據在陣列中都是隨機排列)，
+之後再將這些數據執行trickleDown讓他們符合堆的結構排序，此時這些數據還不是完全有序，但是每一個父節點的數據都會比子結點的數據大。
+在完成基本的堆結構排序後再以remove和insertAt的方式將節點放回堆陣列中完成排序。
 
+因為他insert和remove方法操作的時間複雜度都是O(logN)，而且每個方法都要執行N次，所以整個排序的操作需要O(N*logN)的時間。
+這跟快速排序差不多，但是快速排序比他更有效率，因為trickleDown裡面的while循環中的側做比較多。
+
+for(j=0; j<size; j++)
+{
+	int random = (int)(java.lang.Math.random()*100);
+	Node newNode = new Node(random);
+	theHeap.insertAt(j, newNode);
+	theHeap.incrementSize();
+}
+
+在範例中的一開始我們先隨意插入數據進入堆陣列中，並且在插入的過程中持續增加堆陣列的中數據的數量。
+
+for(j = size/2-1; j>=0; j--)
+{
+	theHeap.trickleDown(j);
+}
+
+再插入完所有數據後，我們會先執行一次trickleDown，讓這個陣列中的數據符合堆陣列的規則，就是每一個父節點的數據都會比子結點的數據大。
+
+for(j=size-1; j>=0; j--)
+{
+	Node biggestNode = theHeap.remove();
+	theHeap.insertAt(j, biggestNode);
+}
+
+最後再用刪除節點的方式取得最大節點並放回堆中。
+
+public Node remove()
+{
+	Node root = heapArray[0];
+	heapArray[0] = heapArray[--currentSize];
+	trickleDown(0);
+	return root;
+}
+在remove的操作中，我們會將根節點放入變數root返回，並且用最後一個未處理數據取代跟節點的數據執行tricleDown。
+未處理的數據就是重新插回陣列的數據，他為由最大的數據排在陣列的最後端依序一直被重新插回陣列中，
+這些數據再重新放回陣列中就已經完成排序不會在移動，取代根節點數據的位置會在每一次的刪除中依序往前，
+所以在堆排序完成時我們可以得到一組升冪排序的陣列。
+
+堆排序的時間複雜度為O(N*logN)。雖然他比快速排序慢一點，但是他比快速排序更穩定。在某些狀況下，快速排序運行的時間複雜度可能會降到O(N^2)，
+然而堆排序的時間可以穩定的維持在O(N*logN)。
+*/
 
 
 
